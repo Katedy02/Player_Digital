c99ab7d72ef87dcfe7decd0584ca5dbcc9ac2de3
Katedy02 <137890205+Katedy02@users.noreply.github.com>
2023-06-28 11:57:31 +0300

    first

--- app/src/main/java/com/example/player_digital/MainActivity.java
+++ app/src/main/java/com/example/player_digital/MainActivity.java
@@ -10,9 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        print("asfafsd");
     }
 
-    private void print(String asfafsdwsdgsdgf) {
-    }
+
 }
