a39b859ea5c9991bb625e6fd19085ded4fc98c65
Katedy02 <137890205+Katedy02@users.noreply.github.com>
2023-07-01 00:53:46 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/player_digital/MainActivity.java
+++ app/src/main/java/com/example/player_digital/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.player_digital;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.player_digital.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvTime, tvDuration;
     SeekBar seekBarTime, seekBarVolume;
-    ImageButton play;
+    ImageButton btnPlay;
 
     MediaPlayer Player_Digital;
     Thread updateThread;
@@ -27,19 +30,24 @@ public class MainActivity extends AppCompatActivity {
         tvDuration=findViewById(R.id.tvDuration);
         seekBarTime=findViewById(R.id.seekBarTime);
         seekBarVolume=findViewById(R.id.seekBarVolume);
-        play = findViewById(R.id.play);
+        btnPlay = findViewById(R.id.btnPlay);
 
         Player_Digital = MediaPlayer.create(this, R.raw.bad_bunny_mayores);
         Player_Digital.setLooping(true);
         Player_Digital.seekTo(0);
-        Player_Digital.setVolume(0.5f,0.5f);
+        Player_Digital.setVolume(0.2f,0.2f);
 
-    //    play.setOnClickListener(this);
+
+        String duration = millisecondsToString(Player_Digital.getDuration());
+        tvDuration.setText(duration);
+
+        btnPlay.setOnClickListener(this);
+
         seekBarVolume.setProgress(50);
         seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean isFromUser) {
-                float volume=progress /100f;
+                float volume = progress / 100f;
                 Player_Digital.setVolume(volume,volume);
             }
 
@@ -53,9 +61,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        seekBarTime.setMax(Player_Digital.getDuration());
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean isFromUser) {
+                if(isFromUser) {
+                    Player_Digital.seekTo(progress);
+                    seekBar.setProgress(progress);
+                }
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-        Player_Digital.start();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         updateThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -65,8 +90,9 @@ public class MainActivity extends AppCompatActivity {
                         public void run() {
                             updateUI();
                         }
+                        private void updateUI() {
+                        }
                     });
-
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -75,25 +101,58 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        updateThread.start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (Player_Digital != null) {
+                    if(Player_Digital.isPlaying()) {
+                        try {
+                            final double current = Player_Digital.getCurrentPosition();
+                            final String elapsedTime = millisecondsToString((int) current);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTime.setText(elapsedTime);
+                                    seekBarTime.setProgress((int) current);
+                                }
+                            });
+                            Thread.sleep(1000);
+                        }catch (InterruptedException e) {}
+                    }
+                }
+            }
+        }).start();
+    }
+    public String millisecondsToString(int time) {
+        String elapsedTime = "";
+        int minutes = time / 1000 / 60;
+        int seconds = time / 1000 % 60;
+        elapsedTime = minutes+":";
+        if(seconds < 10) {
+            elapsedTime += "0";
+        }
+        elapsedTime += seconds;
+        return  elapsedTime;
     }
 
-    private void updateUI() {
-        int current = Player_Digital.getCurrentPosition();
-        int duration = Player_Digital.getDuration();
-
-        tvTime.setText(millisecondsToString(current));
-        tvDuration.setText(millisecondsToString(duration));
-        seekBarTime.setMax(duration);
-        seekBarTime.setProgress(current);
-    }
-
-    private String millisecondsToString(int milliseconds) {
-        int seconds = (milliseconds / 1000) % 60;
-        int minutes = (milliseconds / (1000 * 60)) % 60;
-        return String.format("%02d:%02d", minutes, seconds);
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnPlay) {
+            if(Player_Digital.isPlaying()) {
+                // is playing
+                Player_Digital.pause();
+                btnPlay.setBackgroundResource(R.drawable.ic_play);
+            } else {
+                // on pause
+                Player_Digital.start();
+                btnPlay.setBackgroundResource(R.drawable.pause);
+            }
+        }
     }
 }
+
+
 
 
 
